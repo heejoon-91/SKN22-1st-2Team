fe3101acfab57ef6554f5404d82941c065dbc60b
999ym <yeonmi47@gmail.com>
2025-11-13 08:50:39 +0900

    Feat: layout.py파일 추가, 전체 layout app.py및 각 page별로 적용

--- pages/1_find_station.py
+++ pages/1_find_station.py
@@ -2,3 +2,7 @@ import streamlit as st
 import layout
 
 layout.base_layout()
+
+
+st.write('충전소 찾기 페이지')
+
--- pages/2_diff_charge.py
+++ pages/2_diff_charge.py
@@ -2,3 +2,5 @@ import streamlit as st
 import layout
 
 layout.base_layout()
+
+st.write('요금 비교 페이지')
